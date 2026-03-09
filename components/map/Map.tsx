a6b95490d139f0d9058521f88b3a28dd1fd80546
adamekholt <adam.ekholt@hotmail.com>
2026-03-09 19:42:54 +0100

    chore: update dependencies and add pull request template

---
+++ components/map/Map.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
+
+export default function Map() {
+  useEffect(() => {
+    const map = L.map("map").setView([59.91, 10.75], 13)
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map)
+
+    return () => {
+      map.remove()
+    }
+  }, [])
+
+  return <div id="map" className="w-full h-screen"></div>
+}
