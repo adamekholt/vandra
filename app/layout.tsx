8d973a327747e286b7899baf87ef040e44cb5939
adamekholt <adam.ekholt@hotmail.com>
2026-03-11 12:04:05 +0100

    chore: update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 //denne er ikke ferdigstilt, fortsett senere. Per nå er den samme som i 
 import "leaflet/dist/leaflet.css"
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import './globals.css'
 
 export default function RootLayout ({
