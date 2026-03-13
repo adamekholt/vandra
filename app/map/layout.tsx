0834fb9c0e171c6822f60263f4c61d3e688f6976
adamekholt <adam.ekholt@hotmail.com>
2026-03-13 11:25:29 +0100

    fix(map): move leafletimport

--- app/map/layout.tsx
+++ app/map/layout.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-
 export default function Layout({
   children,
 }: {
--- components/features/Map.tsx
+++ components/features/Map.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+
 import { MapContainer, LayersControl } from "react-leaflet";
 
 import OutdoorLayer from "./map/layers/OutdoorLayer";
