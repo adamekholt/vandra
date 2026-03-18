bf93eae57fe7ec9d769fdc8099cfe7eefb38cfdc
adamekholt <adam.ekholt@hotmail.com>
2026-03-18 16:24:55 +0100

    feat: add marker clustering with start_point support

--- components/features/map/layers/TrailLayer.tsx
+++ components/features/map/layers/TrailLayer.tsx
@@ -1,26 +1,37 @@
-"use client"
-import { GeoJSON } from "react-leaflet"
-import { useEffect, useState } from "react"
+"use client";
 
-export default function TrailLayer() {
+import { GeoJSON } from "react-leaflet";
+import { useEffect, useState } from "react";
 
-  const [data,setData] = useState<any>(null)
+export default function TrailLayer({
+  selectedTrailId,
+}: {
+  selectedTrailId: string | null;
+}) {
+
+  const [data,setData] = useState<any>(null);
 
   useEffect(() => {
     fetch("/api/trails")
-      .then(res => res.json())
-      .then(setData)
-  }, [])
+      .then((res) => res.json())
+      .then(setData);
+  }, []);
 
-  if (!data) return null
+  if (!data || !selectedTrailId) return null;
+  const filteredData = {
+    ...data,
+    features: data.features.filter(
+      (f: any) => f.properties.id === selectedTrailId
+    ),
+  };
 
   return (
     <GeoJSON
-      data={data}
+      data={filteredData}
       style={{
         color: "#e27c00",
         weight: 3
       }}
     />
-  )
+  );
 }
