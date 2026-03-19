9d4a5c7ea1ac1268287a6bed7418301be90a006b
adamekholt <adam.ekholt@hotmail.com>
2026-03-19 15:55:18 +0100

    feat: update markers to use the luciede icons

--- components/features/Map.tsx
+++ components/features/Map.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-
 import { MapContainer, LayersControl } from "react-leaflet";
 import { useState } from "react";
 import OutdoorLayer from "./map/layers/OutdoorLayer";
