e8dd269e4013593f00e6553b5ce2339b57b90f16
adamekholt <adam.ekholt@hotmail.com>
2026-03-29 19:08:36 +0200

    chore: add temporary mock data for landing page. cleanup unused files

--- app/discover/trail/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page({ params }: Readonly<{ params: { id: string } }>) {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      {params.id}
-    </div>
-  )
-}
