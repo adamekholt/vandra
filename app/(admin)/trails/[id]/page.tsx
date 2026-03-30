9d3c3773d10aa00624c3cda97d51067cf6c94c7d
adamekholt <adam.ekholt@hotmail.com>
2026-03-30 12:08:18 +0200

    feat: add trail table and detail page ui

--- app/(admin)/trails/[id]/page.tsx
+++ app/(admin)/trails/[id]/page.tsx
@@ -1,7 +1,11 @@
+import { Navbar } from "@/components/layout/navbar"
+import { Header } from "@/components/layout/header"
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Trail page</h1>
+      <Header/>
+      <Navbar/>
     </main>
   )
 }
--- app/(admin)/trails/new/page.tsx
+++ app/(admin)/trails/new/page.tsx
@@ -1,10 +1,5 @@
 import NewTrailClient from "@/components/features/map/NewTrailClient";
 
 export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Create trail</h1>
-      <NewTrailClient />
-    </main>
-  );
+  return <NewTrailClient />;
 }
