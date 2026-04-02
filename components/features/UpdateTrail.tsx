7b101ed7d6e21adbcf51803786199a32ddb22474
adamekholt <adam.ekholt@hotmail.com>
2026-04-02 17:11:08 +0200

    feat: add edit trail handler

---
+++ components/features/UpdateTrail.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import UpdateTrailForm from "../layout/form/UpdateTrailForm";
+
+export default function EditTrail() {
+  const params = useParams();
+  const id = params.id as string;
+
+  return (
+    <div className="p-8 space-y-4">
+      <div className="mb-6 mt-6">
+        <h1 className="text-3xl font-heading">Uppdatera led</h1>
+      </div>
+      <UpdateTrailForm id={id} />
+    </div>
+  )
+}
